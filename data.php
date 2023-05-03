955923dbd2683d6481a2550e34d80b1542bfdc29
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-05-03 21:07:49 +0200

    print with lenght

--- data.php
+++ data.php
@@ -3,4 +3,8 @@
 $text = $_POST['text'];
 $ban = $_POST['banword'];
 
-echo str_replace($ban, "***", $text);
+echo $text . '(' . strlen($text) . ')' .'<br>' ;
+
+$newText = str_replace($ban, "***", $text);
+
+echo $newText . '(' . strlen($newText) . ')';
